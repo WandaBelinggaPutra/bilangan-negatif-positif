1c55b7aa7c3d15ea34e3f1491d771605b885cb9a
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 20:47:28 +0700

    Add files via upload

---
+++ bilangan negatif positif.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,MAX=0;
+
+cout<<"program menentukan bilangan negatif dan positif"<<endl;
+cout<<"masukan bilangan"<<endl;
+cin>>a;
+
+if(a > MAX)
+	cout<<"maka bilangan tersebut positif";
+	else
+		cout<<"maka bilangan tersebut negatif";
+	return 0;
+	
+}
